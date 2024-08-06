a03b344712cbb6c54e952b26773bd345e086f685
Kukuliak-Dmytro <kukuliak.dmytro.clg@chnu.edu.ua>
2024-08-06 08:52:06 +0300

    Revert "Trying to restore the justice"
    This reverts commit 18ba0ddfe47dcdde5f36ef3b24a9b1a76ebb9251.

--- src/header.tsx
+++ src/header.tsx
@@ -1,5 +1,5 @@
 // import styles from './components/Header/Header.module.css';
-import PrimaryBtn from '../button/PrimaryBtn'
+import PrimaryBtn from './components/button/PrimaryBtn'
 export function Header(){
     return(
     <div >
